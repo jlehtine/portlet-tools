972e832f4f31e68fb216f94254e820dc7d2e6bcb
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2010-04-02 20:56:48 +0000

    Initial files for jetty-pluto-maven-plugin.

---
+++ jetty-pluto-maven-plugin/src/main/java/net/jlehtinen/maven/jettypluto/JettyPlutoMavenPlugin.java
@@ -0,0 +1,12 @@
+package net.jlehtinen.maven.jettypluto;
+
+/**
+ * Run the portlet being developed in a Pluto container under Jetty without the time consuming
+ * deploy process.
+ * 
+ * @goal run
+ */
+public class JettyPlutoMavenPlugin {
+
+	
+}
