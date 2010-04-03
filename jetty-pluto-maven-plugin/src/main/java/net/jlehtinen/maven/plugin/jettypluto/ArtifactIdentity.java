c7b233ffd63489f85d8ac9c20c01e7c2bae7ced8
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2010-04-03 22:06:36 +0000

    Added README.txt and legal comments.

--- jetty-pluto-maven-plugin/src/main/java/net/jlehtinen/maven/plugin/jettypluto/ArtifactIdentity.java
+++ jetty-pluto-maven-plugin/src/main/java/net/jlehtinen/maven/plugin/jettypluto/ArtifactIdentity.java
@@ -1,5 +1,27 @@
 package net.jlehtinen.maven.plugin.jettypluto;
 
+/*
+ * Copyright (c) 2010 Johannes Lehtinen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import java.io.File;
 import java.text.MessageFormat;
 
@@ -20,8 +42,8 @@ public class ArtifactIdentity {
 	/** Version number */
 	protected String version;
 	
-	/** Type of the artifact */
-	protected String type;
+	/** Packaging type */
+	protected String packaging;
 	
 	/** Direct path to a local file containing the artifact */
 	protected String file;
@@ -44,7 +66,7 @@ public class ArtifactIdentity {
 		this.groupId = groupId;
 		this.artifactId = artifactId;
 		this.version = version;
-		this.type = type;
+		this.packaging = type;
 	}
 	
 	/**
@@ -75,12 +97,12 @@ public class ArtifactIdentity {
 	}
 
 	/**
-	 * Returns the type of the artifact.
+	 * Returns the packaging type.
 	 * 
-	 * @return type of the artifact
+	 * @return packaging type
 	 */
-	public String getType() {
-		return type == null ? "jar" : type;
+	public String getPackaging() {
+		return packaging == null ? "jar" : packaging;
 	}
 
 	/**
--- jetty-pluto-maven-plugin/src/main/java/net/jlehtinen/maven/plugin/jettypluto/JettyPlutoRunMojo.java
+++ jetty-pluto-maven-plugin/src/main/java/net/jlehtinen/maven/plugin/jettypluto/JettyPlutoRunMojo.java
@@ -1,5 +1,27 @@
 package net.jlehtinen.maven.plugin.jettypluto;
 
+/*
+ * Copyright (c) 2010 Johannes Lehtinen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -23,12 +45,12 @@ import org.mortbay.jetty.security.HashUserRealm;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
- * Run the portlet being developed in a Pluto container under Jetty without the time consuming
- * deploy process.
+ * Run the portlet being developed in the Apache Pluto container under Jetty without the time
+ * consuming deploy process.
  * 
  * @extendsPlugin jetty
  * @goal run
- * @description Runs the Pluto portal under Jetty directly on a Maven portlet project
+ * @description Runs the Apache Pluto portal under Jetty on a Maven portlet project
  */
 public class JettyPlutoRunMojo extends Jetty6RunMojo {
 
@@ -83,7 +105,7 @@ public class JettyPlutoRunMojo extends Jetty6RunMojo {
      *   &lt;groupId>org.apache.portals.pluto&lt;/groupId>
      *   &lt;artifactId>pluto-portal&lt;/artifactId>
      *   &lt;version>2.0.0&lt;/version>
-     *   &lt;type>war&lt;/type>
+     *   &lt;packaging>war&lt;/packaging>
      * &lt;/portal>
      * </pre>
      * 
@@ -109,7 +131,7 @@ public class JettyPlutoRunMojo extends Jetty6RunMojo {
 	 *     &lt;groupId>org.apache.portals.pluto&lt;/groupId>
 	 *     &lt;artifactId>pluto-container-api&lt;/artifactId>
 	 *     &lt;version>2.0.0&lt;/version>
-	 *     &lt;type>jar&lt;/type>
+	 *     &lt;packaging>jar&lt;/packaging>
 	 *   &lt;/library>
 	 *   &lt;library>
 	 *     &lt;file>/opt/j2ee/libs/portlet-api-2.0.jar&lt;/file>
@@ -343,7 +365,7 @@ public class JettyPlutoRunMojo extends Jetty6RunMojo {
     			aid.getArtifactId(),
     			aid.getVersion(),
     			"runtime",
-    			aid.getType()
+    			aid.getPackaging()
     	);
     }
     
